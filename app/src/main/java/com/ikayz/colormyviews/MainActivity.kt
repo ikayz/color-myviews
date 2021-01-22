d797bf7d32a6c415deaf7d29c151394ca9804e76
Isaac Miti <ikayz@live.com>
2021-01-22 15:54:01 +0200

    Add missing IDs of views

--- app/src/main/java/com/ikayz/colormyviews/MainActivity.kt
+++ app/src/main/java/com/ikayz/colormyviews/MainActivity.kt
@@ -9,12 +9,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setListners()
+        setListeners()
     }
 
-    private fun setListners() {
+    private fun setListeners() {
+        val box_one_text = findViewById<View>(R.id.box_one_text)
+        val box_two_text = findViewById<View>(R.id.box_two_text)
+        val box_three_text = findViewById<View>(R.id.box_three_text)
+        val box_four_text = findViewById<View>(R.id.box_four_text)
+        val box_five_text = findViewById<View>(R.id.box_five_text)
+        val constraint_layout = findViewById<View>(R.id.constraint_layout)
+
         val clickableViews: List<View> =
-                listOf(box_one_text, box_two_text, box_three_text, box_four_text, box_five_text)
+                listOf(box_one_text, box_two_text, box_three_text, box_four_text,
+                        box_five_text, constraint_layout)
 
         for (item in clickableViews) {
             item.setOnClickListener { makeColored(it) }
@@ -31,6 +39,8 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_purple)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_orange_light)
+
+            else -> view.setBackgroundColor(Color.CYAN)
         }
     }
 }
