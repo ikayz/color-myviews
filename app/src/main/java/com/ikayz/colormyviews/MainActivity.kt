068cf00e3a55581471183bd8197777477ebd6ff5
Isaac Miti <ikayz@live.com>
2021-01-22 19:52:24 +0200

    Add code to change box colours from button clicks

--- app/src/main/java/com/ikayz/colormyviews/MainActivity.kt
+++ app/src/main/java/com/ikayz/colormyviews/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.ikayz.colormyviews
 
+import android.annotation.SuppressLint
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
 class MainActivity : AppCompatActivity() {
+    val box_one_text = findViewById<View>(R.id.box_one_text)
+    val box_two_text = findViewById<View>(R.id.box_two_text)
+    val box_three_text = findViewById<View>(R.id.box_three_text)
+    val box_four_text = findViewById<View>(R.id.box_four_text)
+    val box_five_text = findViewById<View>(R.id.box_five_text)
+    val constraint_layout = findViewById<View>(R.id.constraint_layout)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -13,13 +21,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setListeners() {
-        val box_one_text = findViewById<View>(R.id.box_one_text)
-        val box_two_text = findViewById<View>(R.id.box_two_text)
-        val box_three_text = findViewById<View>(R.id.box_three_text)
-        val box_four_text = findViewById<View>(R.id.box_four_text)
-        val box_five_text = findViewById<View>(R.id.box_five_text)
-        val constraint_layout = findViewById<View>(R.id.constraint_layout)
-
         val clickableViews: List<View> =
                 listOf(box_one_text, box_two_text, box_three_text, box_four_text,
                         box_five_text, constraint_layout)
@@ -29,6 +30,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    @SuppressLint("ResourceAsColor")
     private fun makeColored(view: View) {
         when (view.id) {
             // Set the background colour of the boxes on tap
@@ -39,6 +41,11 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_purple)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_orange_light)
+
+            // Buttons which are changing the colour of the boxes
+            R.id.red_button -> box_three_text.setBackgroundColor(R.color.red)
+            R.id.yellow_button -> box_four_text.setBackgroundColor(R.color.yellow)
+            R.id.green_button -> box_five_text.setBackgroundColor(R.color.green)
 
             else -> view.setBackgroundColor(Color.CYAN)
         }
